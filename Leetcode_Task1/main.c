6e97aa0f57c60d32e206502d80ceeba28ccdb6cc
dalbaeb3000 <semen.balashov.02@gmail.com>
2024-08-26 15:08:55 +0300

    Task 2 with shitful main

--- Leetcode_Task1/main.c
+++ Leetcode_Task1/main.c
@@ -30,5 +30,5 @@ int main() {
         printf("%d ",sumIndices[i]);
     }
     free(sumIndices);
-
+    return 0;
 }
--- Leetcode_Task2/main.c
+++ Leetcode_Task2/main.c
@@ -1,47 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct linkedList {
-    int value;
-    linkedList* next;
-}linkedList;
-linkedList* list_init(int value) {
-    linkedList* tmp = (linkedList*)malloc(sizeof(linkedList));
-    tmp->value = value;
+struct ListNode {
+    int val;
+    struct ListNode* next;
+};
+struct ListNode* list_init(int val) {
+    struct ListNode* tmp = (struct ListNode*)malloc(sizeof(struct ListNode));
+    tmp->val = val;
     tmp->next = NULL;
     return tmp;
 }
-void list_add(linkedList* list, int value) {
-    list->next = (linkedList*)malloc(sizeof(linkedList));
-    list = list->next;
-    list->value = value;
-    list->next = NULL;
+void list_add(struct ListNode* list, int val) {
+    struct ListNode* last = list;
+    while (last->next) last = last->next;
+    last->next = (struct ListNode*)malloc(sizeof(struct ListNode));
+    last = last->next;
+    last->val = val;
+    last->next = NULL;
 }
-void checkingcf(int num1, int num2, int* cf) {
-    if (num1 + num2 + *cf > 9) *cf = 1;
-    else *cf = 0;
-}
-linkedList* addTwoNumbers(linkedList* l1, linkedList* l2) {
-    int cf = 0;
-    checkingcf(l1->value, l2->value, &cf);
-    // if (l1 -> value + l2 -> value > 9) {
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    // if (l1 -> val + l2 -> val > 9) {
     //     cf = 1;
     // }
-    linkedList* result = list_init((l1->value + l2->value)%10);
-    while ((l1->next != NULL) && (l2->next != NULL)) {
-        int sum = 0;
-        sum = l1->value + l2->value + cf;
-        if (sum > 9) cf = 1;
-        l1 = l1 -> next;
-        l2 = l2 -> next;
-        list_add(result,sum);
+    int cf = (l1->val + l2->val)/10;
+    struct ListNode* result = list_init((l1->val + l2->val)%10);
+    while ((l1) || (l2)) {
+        if (l1) l1 = l1 -> next;
+        if (l2) l2 = l2 -> next;
+        int sum = cf;
+        if (l1) sum+=l1->val;
+        if (l2) sum+=l2->val;
+        if ((sum != 0) || (l1) || (l2)) list_add(result,sum%10);
+        cf = sum/10;   
     }
-    if (l1 -> value + l2 -> value > 9) {
-        cf = 1;
+    return result;
+}
+void freeList(struct ListNode* list) {
+    while (list->next != NULL) {
+        struct ListNode* temp = list->next;
+        free(list);
+        list = temp;
     }
-    list_add(result, (l1 -> value + l2 -> value + cf)%10);
-    if (l1->next == NULL) {
-        while (l2->next != NULL) {
-
+    free(list);
+}
+int main() {
+    int digit;
+    char tmp;
+    scanf("%d%c", &digit, &tmp);
+    struct ListNode* l1 = list_init(digit);
+    if (tmp != '\n') {
+        while (scanf("%d%c", &digit, &tmp) && tmp != '\n') {
+            list_add(l1, digit);
         }
     }
+    scanf("%d%c", &digit, &tmp);
+    struct ListNode* l2 = list_init(digit);
+    if (tmp != '\n') {
+        while (scanf("%d%c", &digit, &tmp) && tmp != '\n') {
+            list_add(l2, digit);
+        }
+    }
+    struct ListNode* result = addTwoNumbers(l1, l2);
+    while (l1->next != NULL) {
+        struct ListNode* temp = l1->next;
+        free(l1);
+        l1 = temp;
+    }
+    free(l1);
+    while (l2->next != NULL) {
+        struct ListNode* temp = l2->next;
+        free(l2);
+        l2 = temp;
+    }
+    free(l2);
+    while (result->next) {
+        printf("%d ", result->val);
+        struct ListNode* temp = result->next;
+        free(result);
+        result = temp;
+    }
+    printf("%d", result->val);
+    free(result);
+    // freeList(l2);
 }
