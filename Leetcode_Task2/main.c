695bbe67d2e64823841dded547087a7ad2ad5200
dalbaeb3000 <semen.balashov.02@gmail.com>
2024-08-26 13:00:17 +0300

    Task 2 linkedList staff

---
+++ Leetcode_Task2/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct linkedList {
+    int value;
+    linkedList* next;
+}linkedList;
+linkedList* list_init(int value) {
+    linkedList* tmp = (linkedList*)malloc(sizeof(linkedList));
+    tmp->value = value;
+    tmp->next = NULL;
+    return tmp;
+}
+void list_add(linkedList* list, int value) {
+    list->next = (linkedList*)malloc(sizeof(linkedList));
+    list = list->next;
+    list->value = value;
+    list->next = NULL;
+}
+linkedList* addTwoNumbers(linkedList* l1, linkedList* l2) {
+    int cf = 0;
+    if (l1 -> value + l2 -> value > 9) {
+        cf = 1;
+    }
+    linkedList* result = list_init((l1->value + l2->value)%10);
+    while ((l1->next != NULL) && (l2->next != NULL)) {
+        int sum = 0;
+        sum = l1->value + l2->value + cf;
+        if (sum > 9) cf = 1;
+        l1 = l1 -> next;
+        
+
+    }
+}
